254f3c8d0da2d5869f6a2076bc8cbb5868170427
shahendamagdy97‏ <shahendamagdy97@yahoo.com>
2023-08-17 00:38:50 +0300

    Second Update

--- 0x04-python-more_data_structures/Python.h
+++ 0x04-python-more_data_structures/Python.h
@@ -4,23 +4,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-def square_matrix_simple(matrix=[])
-def search_replace(my_list, search, replace)
-def uniq_add(my_list=[])
-def common_elements(set_1, set_2)
-def only_diff_elements(set_1, set_2)
-def number_keys(a_dictionary)
-def print_sorted_dictionary(a_dictionary)
-def update_dictionary(a_dictionary, key, value)
-def simple_delete(a_dictionary, key="")
-def multiply_by_2(a_dictionary)
-def best_score(a_dictionary)
-def multiply_list_map(my_list=[], number=0)
-def roman_to_int(roman_string)
-def weight_average(my_list=[])
-def square_matrix_map(matrix=[])
-def complex_delete(a_dictionary, value)
-void print_python_list(PyObject *p)
-void print_python_bytes(PyObject *p)
+void square_matrix_simple(int matrix[]);
+void search_replace(int my_list[], int search, int replace);
+void uniq_add(int my_list[]);
+void common_elements(int set_1[], int set_2[]);
+void only_diff_elements(int set_1[], int set_2[]);
+int number_keys(int a_dictionary[]);
+void print_sorted_dictionary(int a_dictionary[]);
+void update_dictionary(int a_dictionary[], int key, int value);
+void simple_delete(int a_dictionary[], int key);
+void multiply_by_2(int a_dictionary[]);
+int best_score(int a_dictionary[]);
+void multiply_list_map(int my_list[], int number);
+int roman_to_int(char roman_string[]);
+float weight_average(float my_list[]);
+void square_matrix_map(int matrix[]);
+void complex_delete(int a_dictionary[], int value);
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
 
 #endif /* PYTHON_H */
